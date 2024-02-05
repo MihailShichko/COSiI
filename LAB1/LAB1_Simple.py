bda54969af77a37a397f260bea57273632721c1b
MikhailShychko <mihalshichko@gmail.com>
2024-02-05 21:29:35 +0300

    random func check

--- LAB1/LAB1_Simple.py
+++ LAB1/LAB1_Simple.py
@@ -3,6 +3,14 @@ import numpy as np
 
 fig, ax = plt.subplots()
 ax.set_title("")
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.grid()
+
+x = np.linspace(-10, 10, 100)
+y = np.cos(3*x) + np.sin(2*x)
+
+ax.plot(x,y)
 
 plt.show()
 
